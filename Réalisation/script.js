e89939b2bf7fb58c4b7427d9fe94874d6a87c2ac
Omar Mouttalie <omarmouttalie@gmail.com>
2025-10-23 10:56:13 +0100

    Réalisation To-Do List

--- Réalisation/script.js
+++ Réalisation/script.js
@@ -18,6 +18,7 @@ button.addEventListener("click", function addTask() {
   saveData();
 })
 
+
 listContainer.addEventListener("click", function(e) {
   if(e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
